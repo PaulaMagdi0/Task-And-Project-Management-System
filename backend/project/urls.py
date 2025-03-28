b6b1772270fea63ef601184d71b7b8b963a1a1ef
HossamZakaria <hosamking2122@gmail.com>
2025-03-28 23:51:43 +0200

    merging

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     path("api/assignments/", include("apps.assignments.urls")),
     path("api/grades/", include("apps.grades.urls")),
     path('api/users/', include('apps.users.urls')),  # Add this line
-
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import Navbar from './pages/Navbar';
+import SignIn from './pages/Signin';
+import SignUp from './pages/Signup';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        // <BrowserRouter>
+        // <Header /> //navbar to be shown in all pages
+        <BrowserRouter>
+            {/* <Navbar /> */}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+
+    );
 }
 
-export default App
+export default App;
