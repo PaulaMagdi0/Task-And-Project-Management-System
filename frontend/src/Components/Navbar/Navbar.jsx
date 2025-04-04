57cb12e172b492e48fb6d1eabe7a79292ca616cf
Paula Magdy <paulamagdy665@gmail.com>
2025-04-04 04:20:39 +0200

    contact

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import "./Navbar.css"; // Make sure you have the required styles
+import "./Navbar.css";
 import useCustomScripts from "../../Hooks/useCustomScripts";
 
 const Navbar = () => {
   useCustomScripts();
   const location = useLocation();
-
-  const [isMenuOpen, setIsMenuOpen] = useState(false); // State for mobile menu toggle
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // State for dropdown toggle
 
   const navItems = [
     { name: "Home", href: "/" },
@@ -24,16 +21,6 @@ const Navbar = () => {
     },
   ];
 
-  // Toggle mobile menu visibility
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  // Toggle dropdown menu visibility
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-
   return (
     <header id="header" className="branding header fixed-top py-3">
       <div className="d-flex align-items-center">
@@ -41,41 +28,14 @@ const Navbar = () => {
           <Link to="/" className="logo d-flex align-items-center">
             <h1 className="sitename">Task Flow</h1>
           </Link>
-          <nav id="navmenu" className={`navmenu ${isMenuOpen ? "active" : ""}`}>
+          <nav id="navmenu" className="navmenu">
             <ul className="nav-list gap-2">
               {navItems.map((item, index) => (
-                <li
-                  key={index}
-                  className={`nav-item ${item.dropdown ? "dropdown" : ""}`}
-                >
+                <li key={index} className={item.dropdown ? "dropdown" : ""}>
                   {item.dropdown ? (
-                    <>
-                      <span
-                        className="fs-4 ms-3 user-icon"
-                        onClick={toggleDropdown}
-                      >
-                        <i className="bi bi-person-circle"></i>
-                      </span>
-                      {/* Mobile dropdown */}
-                      {isDropdownOpen && (
-                        <ul className="mt-2 dropdown-menu">
-                          {item.dropdown.map((subItem, subIndex) => (
-                            <li key={subIndex}>
-                              <Link
-                                to={subItem.href}
-                                className={
-                                  location.pathname === subItem.href
-                                    ? "active"
-                                    : ""
-                                }
-                              >
-                                {subItem.name}
-                              </Link>
-                            </li>
-                          ))}
-                        </ul>
-                      )}
-                    </>
+                    <span className="fs-4 ms-3 user-icon">
+                      <i className="bi bi-person-circle"></i>
+                    </span>
                   ) : (
                     <Link
                       to={item.href}
@@ -86,13 +46,26 @@ const Navbar = () => {
                       {item.name}
                     </Link>
                   )}
+                  {item.dropdown && (
+                    <ul className="mt-2">
+                      {item.dropdown.map((subItem, subIndex) => (
+                        <li key={subIndex}>
+                          <Link
+                            to={subItem.href}
+                            className={
+                              location.pathname === subItem.href ? "active" : ""
+                            }
+                          >
+                            {subItem.name}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
                 </li>
               ))}
             </ul>
-            <i
-              className="mobile-nav-toggle d-xl-none bi bi-list"
-              onClick={toggleMenu} // Toggle mobile menu on click
-            ></i>
+            <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
           </nav>
         </div>
       </div>
