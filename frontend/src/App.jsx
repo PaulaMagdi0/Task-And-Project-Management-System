7d0f8340acb05444b6ef2b1362955f136d8fc269
MaNn0 <mlolo80013@gmail.com>
2025-04-08 00:07:19 +0400

    merge paula mano

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,3 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import useCustomScripts from "./Hooks/useCustomScripts";
-import Navbar from "./Components/Navbar/Navbar";
-import Footer from "./Components/Footer/Footer";
-import ScrollToTop from "./Components/ScrollToTop";
-import NotFound from "./Components/NotFound/NotFound";
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-import Services from "./pages/Services/Services";
-import Contact from "./pages/Contact/Contact";
-import TermsOfService from "./pages/TermsOfService/TermsOfService";
-import PrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
-import Chatting, { dummyUsers } from "./pages/Chat/Chatting";
-
-import "./assets/css/main.css";
-import "aos/dist/aos.css";
-import "./App.css";
-
-const App = () => {
-  useCustomScripts();
-
-  return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Navbar />
-      <main className="main">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/terms" element={<TermsOfService />} />
-          <Route path="/privacy" element={<PrivacyPolicy />} />
-
-          <Route path="/chat" element={<Chatting users={dummyUsers} />} />
-
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      </main>
-      <Footer />
-    </BrowserRouter>
-  );
-};
-
-export default App;
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux'; // Import useSelector
@@ -62,33 +15,49 @@ import Grades from './pages/Instructor/Grades';
 import CreateTask from './pages/Instructor/CreateAssignment';
 import UploadStudentPage from './components/AddStudent';
 
-function App() {
-  // Retrieve userType and role from Redux store
+import "./assets/css/main.css";
+import "aos/dist/aos.css";
+import "./App.css";
+
+const App = () => {
+  useCustomScripts();
+
   const { userType, role } = useSelector((state) => state.auth);
 
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/upload-student" element={<UploadStudentPage />} />
-        <Route path="/student/dashboard" element={<StudentDashboard />} />
-        <Route path="/supervisor/dashboard" element={<SupervisorDashboard />} />
-        <Route path="/branchmanager/dashboard" element={<BranchManagerDashboard />} />
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/terms" element={<TermsOfService />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/upload-student" element={<UploadStudentPage />} />
+          <Route path="/student/dashboard" element={<StudentDashboard />} />
+          <Route path="/supervisor/dashboard" element={<SupervisorDashboard />} />
+          <Route path="/branchmanager/dashboard" element={<BranchManagerDashboard />} />
 
-        {/* Instructor Dashboard Routes */}
-        {userType === 'staff' && role === 'instructor' && (
-          <Route path="/instructor/dashboard" element={<InstructorDashboard />} />
-        )}
-        <Route path="/instructor/assignments" element={<Assignments />} />
-        <Route path="/instructor/submissions" element={<Submissions />} />
-        <Route path="/instructor/grades" element={<Grades />} />
-        <Route path="/instructor/createTask" element={<CreateTask />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          {/* Instructor Dashboard Routes */}
+          {userType === 'staff' && role === 'instructor' && (
+            <Route path="/instructor/dashboard" element={<InstructorDashboard />} />
+          )}
+          <Route path="/instructor/assignments" element={<Assignments />} />
+          <Route path="/instructor/submissions" element={<Submissions />} />
+          <Route path="/instructor/grades" element={<Grades />} />
+          <Route path="/instructor/createTask" element={<CreateTask />} />
+          <Route path="/chat" element={<Chatting users={dummyUsers} />} />
+
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
     </BrowserRouter>
   );
-}
+};
 
 export default App;
