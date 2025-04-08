3974f810106b57d1c6c140e13800d328c82d172b
installer <installer@gmail.com>
2025-04-08 04:53:19 +0200

    DashBoard-staffmember

--- backend/apps/courses/urls.py
+++ backend/apps/courses/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import CourseListView  # Ensure this view exists
+from .views import CourseListView, StaffMemberCoursesView # Ensure this view exists
 
 urlpatterns = [
     path("", CourseListView.as_view(), name="course-list"),
+    # path("<int:id>/", InstructorCoursesView.as_view(), name="course-detail"),
+    path('staffmember/<int:staff_member_id>/courses/', StaffMemberCoursesView.as_view(), name='staffmember-courses'),
+    # http://127.0.0.1:8000/api/courses/staffmember/2/courses/
+
+
 ]
