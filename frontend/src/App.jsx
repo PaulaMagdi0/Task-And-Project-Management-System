1eab08d5b84c8ee6fc61ffd274aaa28bb2bae87e
abdelrahman ramadan <mlolo80013@gmail.com>
2025-05-02 07:07:02 +0300

    enhancing home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 // File: src/App.jsx
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 // Components
 import Navbar from "./components/Navbar";
@@ -18,11 +18,11 @@ import TermsOfService from "./Components/TermsOfService/TermsOfService";
 import Team from "./Components/Team/Team";
 import SignIn from "./pages/Signin";
 import NotFound from "./pages/NotFound";
-import VerifiedPage from './pages/verifiedPage';
+import VerifiedPage from "./pages/verifiedPage";
 import AccessDenied from "./Components/Denied/Denied";
-import RecommendationForm from './Components/Recommendation/Recommendation';
-import UploadStudentPage from './components/AddStudent';
-import ProfilePage from './pages/ProfilePage';
+import RecommendationForm from "./Components/Recommendation/Recommendation";
+import UploadStudentPage from "./components/AddStudent";
+import ProfilePage from "./pages/ProfilePage";
 
 // Dashboard Components
 import Dashboard from "./pages/DashBoard/Dashboard";
@@ -31,26 +31,27 @@ import SupervisorDashboard from "./pages/SupervisorDashBoard/SupervisorDashboard
 import BranchManagerDashboard from "./pages/BranchManagerDashBoard/BranchManagerDashboard";
 import InstructorDashboard from "./pages/Instructor/Dashboard";
 import Course from "./pages/DashBoard/Courses";
-import Assignments from './Components/Assignments/Assignments';
-import Submissions from './pages/DashBoard/Submissions';
-import Grades from './pages/DashBoard/Grades';
-import CreateAssignment from './pages/DashBoard/CreateAssignment';
-import Hello from './pages/DashBoard/hello';
+import Assignments from "./Components/Assignments/Assignments";
+import Submissions from "./pages/DashBoard/Submissions";
+import Grades from "./pages/DashBoard/Grades";
+import CreateAssignment from "./pages/DashBoard/CreateAssignment";
+import Hello from "./pages/DashBoard/hello";
 
 // Admin Dashboard
-import AdminDashboard from './pages/AdminDashboard/Admin';
-import GitHubStat from './Components/githubStat/githubStat';
+import AdminDashboard from "./pages/AdminDashboard/Admin";
+import GitHubStat from "./Components/githubStat/githubStat";
 
 // ** Chat Components **
-import ChatRoomList from './Components/Chat/ChatRoomList';
-import ChatRoomMessages from './Components/Chat/ChatRoomMessages';
+import ChatRoomList from "./Components/Chat/ChatRoomList";
+import ChatRoomMessages from "./Components/Chat/ChatRoomMessages";
 
 // Styles
 import "./assets/css/main.css";
 import "aos/dist/aos.css";
 import "./App.css";
-import NotVerifiedPage from './pages/NotVerifiedPage';
-import AddCourses from './Components/AddCourses/AddCourss';
+import NotVerifiedPage from "./pages/NotVerifiedPage";
+import AddCourses from "./Components/AddCourses/AddCourss";
+import PrivacyPolicy from "./Components/PrivacyPolicy/PrivacyPolicy";
 
 const App = () => {
   useCustomScripts();
@@ -69,6 +70,7 @@ const App = () => {
           <Route path="/contact" element={<Contact />} />
           <Route path="/terms" element={<TermsOfService />} />
           <Route path="/team" element={<Team />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/upload-student" element={<UploadStudentPage />} />
           <Route path="/profile" element={<ProfilePage />} />
@@ -76,9 +78,7 @@ const App = () => {
           <Route path="/recommendation" element={<RecommendationForm />} />
           <Route path="/hello" element={<AddCourses />} />
 
-
           <Route path="/github-stat" element={<GitHubStat />} />
-      
 
           {/* Chat Routes */}
           <Route path="/chat/rooms" element={<ChatRoomList />} />
@@ -104,9 +104,11 @@ const App = () => {
           />
 
           {/* Instructor Dashboard */}
-          {userType === 'staff' && role === 'instructor' && (
-            <Route path="/instructor/dashboard" element={<InstructorDashboard />} />
-
+          {userType === "staff" && role === "instructor" && (
+            <Route
+              path="/instructor/dashboard"
+              element={<InstructorDashboard />}
+            />
           )}
           {/* Instructor-Specific Routes */}
           {userType === "staff" && role === "instructor" && (
@@ -117,7 +119,7 @@ const App = () => {
           )}
 
           {/* Admin Dashboard */}
-          {userType === 'staff' && role === 'admin' && (
+          {userType === "staff" && role === "admin" && (
             <Route path="/admin/dashboard/*" element={<AdminDashboard />} />
           )}
 
